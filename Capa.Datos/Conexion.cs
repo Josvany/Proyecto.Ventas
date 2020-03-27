e3a53079a555fe72350df804be03a6f9eedad9e0
Josvany_G <Josvany_G@JOSVANY>
2020-03-26 19:38:03 -0600

    Agregar archivos de proyecto.

---
+++ Capa.Datos/Conexion.cs
@@ -0,0 +1,22 @@
+﻿namespace Capa.Datos
+{
+    public class Conexion
+    {
+       
+        public static GDatos GDatos;
+        public static bool IniciarSesion(string nombreServidor, string baseDatos, string usuario, string password)
+        {
+            GDatos = new SqlServer(nombreServidor, baseDatos, usuario, password);
+            return GDatos.Autenticar(usuario, password);
+        } //fin inicializa sesion
+
+        public static void FinalizarSesion()
+        {
+            GDatos.CerrarConexion();
+        } //fin FinalizaSesion
+ 
+        
+          
+        
+    }
+}
---
+++ Capa.Datos/encriptacion.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security;
+using System.Security.Cryptography;
+
+namespace Capa.Datos
+{
+    public class Encriptacion
+    {
+        private string key;
+
+        public Encriptacion()
+        {
+            key = "0=/1234#a-z";
+        }
+
+        public string Encriptar(string texto)
+        {
+            byte[] keyArray;
+            byte[] Arreglo_a_Cifrar = UTF8Encoding.UTF8.GetBytes(texto);
+            MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+
+            keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+            hashmd5.Clear();
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            tdes.Key = keyArray;
+            tdes.Mode = CipherMode.ECB;
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateEncryptor();
+
+            byte[] ArrayResultado = cTransform.TransformFinalBlock(Arreglo_a_Cifrar, 0, Arreglo_a_Cifrar.Length);
+            tdes.Clear();
+
+            return Convert.ToBase64String(ArrayResultado, 0, ArrayResultado.Length);
+        }
+
+        public string Desencriptar(string textoEncriptado)
+        {
+            byte[] keyArray;
+            byte[] Array_a_Descifrar = Convert.FromBase64String(textoEncriptado);
+
+            MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+            keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+            hashmd5.Clear();
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            tdes.Key = keyArray;
+            tdes.Mode = CipherMode.ECB;
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateDecryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(Array_a_Descifrar, 0, Array_a_Descifrar.Length);
+
+            tdes.Clear();
+            string res = UTF8Encoding.UTF8.GetString(resultArray);
+            return res;
+        }
+    }
+}
